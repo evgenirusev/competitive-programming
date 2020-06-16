b9844054bcc8d907eac63a9a700ad84f3c826b93
evgenirusev <evgeni.n.rusev@gmail.com>
2020-06-16 23:24:29 +0300

    refactor way too long words solution

--- codeforces/WayTooLongWords.cpp
+++ codeforces/WayTooLongWords.cpp
@@ -6,20 +6,14 @@ int main()
     int numberOfWords;
     cin >> numberOfWords;
 
-    vector<string> words;
     for (int i = 0; i < numberOfWords; i++) {
         string word;
         cin >> word;
-        words.push_back(word);
-    }
 
-    for (string s : words) {
-        if (s.length() > 10) {
-            cout << string(1, s.at(0)) 
-                << (s.length() - 2) 
-                << string(1, s.at(s.length() - 1));
+        if (word.length() > 10) {
+            cout << word.at(0) << word.length() - 2 << word.at(word.length() - 1);
         } else {
-            cout << s;
+            cout << word;
         }
 
         cout << endl;
