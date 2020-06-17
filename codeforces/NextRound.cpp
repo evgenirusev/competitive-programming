f2e1fb7ff016d8433cd813e83858d906ab57ffe0
evgenirusev <evgeni.n.rusev@gmail.com>
2020-06-17 10:33:27 +0300

    add NextRound

---
+++ codeforces/NextRound.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n, k, contestantsThatAdvance;
+    int a[100];
+
+    cin >> n >> k;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] > 0 && a[i] >= a[k - 1]) {
+            contestantsThatAdvance++;
+        }
+    }
+
+    cout << contestantsThatAdvance;
+}
