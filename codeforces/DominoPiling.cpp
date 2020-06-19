d5f9f8294669b65b5f91fcc32ab39d82936ec1e5
evgenirusev <evgeni.n.rusev@gmail.com>
2020-06-19 09:39:32 +0300

    implement DominoPiling.cpp

--- codeforces/DominoPiling.cpp
+++ codeforces/DominoPiling.cpp
@@ -1,8 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main()
 {
-    
+    int m, n;
+    cin >> m >> n;
+ 
+    vector<vector<bool>> rectangle(m, vector<bool>(m, false));
+    int placedDominos = 0;
+ 
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (!rectangle[i][j] && (j + 1 < n && !rectangle[i][j + 1])) {
+                rectangle[i][j] = true;
+                rectangle[i][++j] = true;
+ 
+                placedDominos++;
+            }
+        }
+    }
+ 
+    if (n % 2 != 0) {
+        placedDominos += m / 2;
+    }
+ 
+    cout << placedDominos;
+ 
     return 0;
 }
