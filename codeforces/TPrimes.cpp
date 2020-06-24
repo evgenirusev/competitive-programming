c7f3889fcd3156d193e717ed22e897f4f6809cdd
evgenirusev <evgeni.n.rusev@gmail.com>
2020-06-24 13:21:46 +0300

    add TPrimes

---
+++ codeforces/TPrimes.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string smallest;
+string largest;
+
+long long getDigitsSum(string n) {
+    long long digitsSum = 0;
+    for (int i = 0; i < n.length(); i++) {
+        digitsSum += n.at(i) - 48;
+    }
+
+    return digitsSum;
+}
+
+void compute(long long m, string current, string target, string* result) {
+    if (m <= 0) {
+        return;
+    }
+
+    for (long long i = 0; i < 10; i++) {
+        string currentNum = current + to_string(i);
+
+        if (to_string(getDigitsSum(currentNum)) == target) {
+            *result = currentNum + string(m - 1, '0');
+            return;
+        }
+
+        compute(m - 1, currentNum, target, result);
+
+        if (!result->empty()) {
+            return;
+        }
+    }
+}
+
+void findSmallest(long long m, string target, string* result) {
+    for (long long i = 1; i < 10; i++) {
+        compute(m - 1, to_string(i), target, result);
+
+        if (!smallest.empty()) {
+            break;
+        }
+    }
+}
+
+void findLargest(long long m, string target, string* result) {
+    for (long long i = 9; i >= 1; i--) {
+        compute(m - 1, to_string(i), target, result);
+
+        if (!largest.empty()) {
+            break;
+        }
+    }
+}
+
+void printResult(string str) {
+    if (str.empty()) {
+        cout << -1;
+    } else {
+        cout << str;
+    }
+}
+
+int main()
+{
+    long long m;
+    string target;
+    cin >> m >> target;
+
+    findSmallest(m, target, &smallest);
+    findLargest(m, target, &largest);
+
+    printResult(smallest);
+    cout << " ";
+    printResult(largest);
+
+    return 0;
+}
