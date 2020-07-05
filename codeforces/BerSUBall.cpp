e1b60aa608a15d1bd97ca5e7c08d5ba098bb8743
evgenirusev <evgeni.n.rusev@gmail.com>
2020-07-05 17:15:07 +0300

    add BerSUBall.cpp

---
+++ codeforces/BerSUBall.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m, maxPairs = 0;
+    cin >> n;
+
+    // will initially implement with array, then consider using hash map
+    vector<int> boys = {};
+    for (int i = 0; i < n; i++) {
+        int boySkill;
+        cin >> boySkill;
+        boys.push_back(boySkill);
+    }
+
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        int girlSkill;
+        cin >> girlSkill;
+
+        int bestMatch = -1;
+        for (int j = 0; j < boys.size(); j++) {
+            int diff = abs(boys[j] - girlSkill);
+
+            if (diff == 1) {
+                bestMatch = j;
+                break;
+            }
+
+            if (diff == 0) {
+                bestMatch = j;
+            }
+        }
+
+        if (bestMatch != -1) {
+            maxPairs++;
+            boys.erase(boys.begin() + bestMatch);
+        }
+    }
+
+    cout << maxPairs;
+    return 0;
+}
